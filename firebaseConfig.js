b2d1c107097a591d1e6def39a4fa28e4eb1b2bdd
Dohyuntae <dohyuntae123@gmail.com>
2021-08-12 10:19:57 +0900

    firebase연동2

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+export default{
+		apiKey: "AIzaSyCYwArm4OmtR1a6S6ioLDw0Z6A2dLBJcsY",
+		authDomain: "flaskfirebase-community.firebaseapp.com",
+		databaseURL: "https://flaskfirebase-community-default-rtdb.firebaseio.com",
+		projectId: "flaskfirebase-community",
+		storageBucket: "flaskfirebase-community.appspot.com",
+		messagingSenderId: "838585883340",
+		appId: "1:838585883340:web:63131bb3d0ca3d17804069",
+		measurementId: "G-ZJVCRHCK2G"
+}
+
